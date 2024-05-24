63aab3703d692cf45a07d0b608228b60eeaa544a
TjanKazar <tjan.kazar3@gmail.com>
2024-05-24 17:23:28 +0200

    translation solved

--- ozra_userapp/ozra4/settings.py
+++ ozra_userapp/ozra4/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.locale.LocaleMiddleware',  # Make sure it's included here
+    'django.middleware.locale.LocaleMiddleware',
 ]
 
 
